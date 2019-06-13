7e0941208af0be22ea1a361e887078fdff119c80
gmcrosh <gmcrosh@gmail.com>
2019-06-13 02:48:42 +0000

    More updates

--- install-hyperscan.sh
+++ install-hyperscan.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -ex
-apt-get update
-apt-get install git
-apt-get install cmake
-apt-get install g++
-apt-get install libboost-all-dev
-apt-get install ragel
 
 wget https://github.com/intel/hyperscan/archive/v5.1.1.tar.gz
 tar -xzvf v5.1.1.tar.gz
