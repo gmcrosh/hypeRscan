91f9831c95761f8e8281d3992234814900571b28
gmcrosh <gmcrosh@gmail.com>
2019-06-13 02:31:47 +0000

    More changes

--- install-hyperscan.sh
+++ install-hyperscan.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-
 apt-get update
 apt-get install git
 apt-get install cmake
@@ -14,5 +12,4 @@ tar -xzvf v5.1.1.tar.gz
 
 mkdir build_hyperscan
 cmake ../hyperscan-5.1.1/
-make
 make install
