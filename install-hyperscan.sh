9c512252c1dc948d1c27edf9f024c5dc3386ff90
gmcrosh <gmcrosh@gmail.com>
2019-06-16 17:05:45 -0500

    Updating build instructions

--- install-hyperscan.sh
+++ install-hyperscan.sh
@@ -3,9 +3,9 @@ set -ex
 
 cd ..
 wget https://github.com/intel/hyperscan/archive/v5.1.1.tar.gz
-tar -xzvf v5.1.1.tar.gz
+tar -xfz v5.1.1.tar.gz
 
 mkdir build_hyperscan
 cd build_hyperscan
-cmake ../hyperscan-5.1.1/
+cmake ../hyperscan-5.1.1/ -DBUILD_SHARED_LIBS=ON
 sudo make install
