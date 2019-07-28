19a58ce4d4e0d3fc90eb8a2407a04473670ff10b
Graham <gmcrosh@gmail.com>
2019-07-28 17:52:34 -0500

    Adding deb and updating install script

--- install-hyperscan.sh
+++ install-hyperscan.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -ex
 
-cd ..
-wget https://github.com/intel/hyperscan/archive/v5.1.1.tar.gz
-tar -xzf v5.1.1.tar.gz
+#cd ..
+#wget https://github.com/intel/hyperscan/archive/v5.1.1.tar.gz
+#tar -xzf v5.1.1.tar.gz
 
-mkdir build_hyperscan
-cd build_hyperscan
-cmake ../hyperscan-5.1.1/ -DBUILD_STATIC_AND_SHARED=ON
-sudo make install
-sudo ldconfig
+#mkdir build_hyperscan
+#cd build_hyperscan
+#cmake ../hyperscan-5.1.1/ -DBUILD_STATIC_AND_SHARED=ON
+#sudo make install
+#sudo ldconfig
+sudo dpkg -i hyperscan_5.1.1-1_amd64.deb
